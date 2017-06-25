latex3/latex3

replace basename with a more generic path split
One pattern can return both parts of the path at once, yielding a
function that not only replaces basename but also is usable as its
counterpart, dirname.

Selecting the second of the two return values replaces the old
functionality. A legacy basename function was not retained, as it was
only used in two places, which in a coming commit will also be gone and
replaced with the need to further process both path components - the
ideal case for the new function.

l3build/l3build.lua
@@ -1430,10 +1430,14 @@ function stripext(file)
   return name or file
 end
 
--- Strip the path from a file name (if present)
-function basename(file)
-  local name = match(file, "^.*/([^/]*)$")
-  return name or file
+-- Split a path into file and directory component
+function splitpath(file)
+  local path, name = match(file, "^(.*)/([^/]*)$")
+  if path then
+    return path, name
+  else
+    return ".", file
+  end
 end
 
 -- Look for a test: could be in the testfiledir or the unpackdir
@@ -1528,7 +1532,7 @@ function tex(file)
 end
 
 function typesetpdf(file)
-  local name = stripext(basename(file))
+  local name = stripext(select(2, splitpath(file))
   print("Typesetting " .. name)
   local errorlevel = typeset(file)
   if errorlevel == 0 then
@@ -1545,7 +1549,7 @@ typeset = typeset or function(file)
   if errorlevel ~= 0 then
     return errorlevel
   else
-    local name = stripext(basename(file))
+    local name = stripext(select(2, splitpath(file)))
     errorlevel = biber(name) + bibtex(name)
     if errorlevel == 0 then
       local function cycle(name)

latex3/l3build

l3build.lua
@@ -1417,15 +1417,19 @@ function dvitopdf(name, dir, engine, hide)
   end
 end
 
--- Strip the path from a file name (if present)
-function basename(file)
-  local name = match(file, "^.*/([^/]*)$")
-  return name or file
+-- Split a path into file and directory component
+function splitpath(file)
+  local path, name = match(file, "^(.*)/([^/]*)$")
+  if path then
+    return path, name
+  else
+    return ".", file
+  end
 end
 
 -- Strip the extension from a file name (if present)
 function jobname(file)
-  local name = match(basename(file), "^(.*)%.")
+  local name = match(select(2, splitpath(file)), "^(.*)%.")
   return name or file
 end
 
@@ -1521,7 +1525,7 @@ function tex(file)
 end
 
 function typesetpdf(file)
-  local name = jobname(file)
+  local name = jobname(select(2, splitpath(file))
   print("Typesetting " .. name)
   local errorlevel = typeset(file)
   if errorlevel == 0 then
@@ -1538,7 +1542,7 @@ typeset = typeset or function(file)
   if errorlevel ~= 0 then
     return errorlevel
   else
-    local name = jobname(file)
+    local name = jobname(select(2, splitpath(file)))
     errorlevel = biber(name) + bibtex(name)
     if errorlevel == 0 then
       local function cycle(name)