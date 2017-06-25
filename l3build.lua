latex3/l3build

commit 8b2825f818ff33830fab0d65a95c9ff44e34a798

Ensure typesetdemofiles go to CTAN
Treat as part of typesetfiles for this purpose.

l3build.lua
@@ -1825,6 +1825,12 @@ function bundlectan()
     for _,i in ipairs(typesetfiles) do
       insert(pdffiles, (gsub(i, "%.%w+$", ".pdf")))
     end
+    -- For the purposes here, any typesetting demo files need to be
+    -- part of the main typesetting list
+    local typesetfiles
+    for _,v in pairs(typesetdemofiles) do
+      insert(typesetfiles, v)
+    end
     typesetlist = excludelist(typesetfiles, {sourcefiles})
     sourcelist = excludelist(
       sourcefiles, {bstfiles, installfiles, makeindexfiles}

commit e30d8bb731d75ddff0e03ff07dcb03ca195078dc

Locally copy typesetfiles inside bundlectan() (fixes #3)
This issue was introduced by 8b2825f8.

l3build.lua
@@ -1897,7 +1897,7 @@ function bundlectan()
     end
     -- For the purposes here, any typesetting demo files need to be
     -- part of the main typesetting list
-    local typesetfiles
+    local typesetfiles = typesetfiles
     for _,v in pairs(typesetdemofiles) do
       insert(typesetfiles, v)
     end