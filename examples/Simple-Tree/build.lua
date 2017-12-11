800dbed9dd154fa338b226d51c2b430154e4b4ac
Will Robertson <will@wspr.io>
2017-12-11 22:05:55 +1030

    with a tree structure should create tds zip file
    definitely a use case where the TDS should basically be mandatory
    
    [ci skip]

--- examples/Simple-Tree/build.lua
+++ examples/Simple-Tree/build.lua
@@ -5,6 +5,7 @@ module = "simple-tree"
 sourcefiledir = "code"
 docfiledir    = "doc"
 typesetfiles  = {"*.dtx","*.tex"}
+packtdszip    = true -- recommended for "tree" layouts
 
 kpse.set_program_name("kpsewhich")
 dofile(kpse.lookup("l3build.lua"))
