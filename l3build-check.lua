ad9555ba135815e8e6ef271adb25f080820045d0
Joseph Wright <joseph.wright@morningstar2.co.uk>
2018-01-07 15:34:45 +0000

    Don't clean out localdir
    Got lost in the file split ...

--- l3build-check.lua
+++ l3build-check.lua
@@ -48,7 +48,6 @@ local execute          = os.execute
 -- for saving the test files
 function checkinit()
   cleandir(testdir)
-  cleandir(localdir)
   depinstall(checkdeps)
   -- Copy dependencies to the test directory itself: this makes the paths
   -- a lot easier to manage, and is important for dealing with the log and
