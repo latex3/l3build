095710181a0ebd4a627763a61c341d842488ccaa
Joseph Wright <joseph.wright@morningstar2.co.uk>
2018-09-25 21:55:30 +0100

    Correct a variable name

--- l3build.lua
+++ l3build.lua
@@ -140,8 +140,8 @@ if options["target"] == "check" then
       for _,config in ipairs(failed) do
         print("Failed tests for configuration " .. config .. ":")
         print("\n  Check failed with difference files")
-        if failed ~= "build" then
-          local testdir = testdir .. "-" .. failed
+        if config ~= "build" then
+          local testdir = testdir .. "-" .. config
         end
         for _,i in ipairs(filelist(testdir,"*" .. os_diffext)) do
           print("  - " .. testdir .. "/" .. i)
