a8d17f35d40689ba14c71163cb30f0fb04a5ca2f
Joseph Wright <joseph.wright@morningstar2.co.uk>
2018-12-21 23:26:13 +0000

    Load correct file data

--- l3build-upload.lua
+++ l3build-upload.lua
@@ -87,7 +87,7 @@ function upload(tagnames)
 
   -- Get data from command line if appropriate
   if options["file"] then
-    local f = open(file,"r")
+    local f = open(options["file"],"r")
     uploadconfig.announcement = assert(f:read('*a'))
     close(f)
   end
