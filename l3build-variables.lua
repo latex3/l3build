3da97dfd4a305732fcc51f5cfcc5b5dbe05f7731
Marcel Fabian Krüger <tex@2krueger.de>
2020-12-05 14:37:13 +0100

    Use metatable-less default for test_types

--- l3build-variables.lua
+++ l3build-variables.lua
@@ -207,22 +207,22 @@ pvtext = pvtext or ".pvt"
 tlgext = tlgext or ".tlg"
 tpfext = tpfext or ".tpf"
 
-test_types = setmetatable(test_types or {}, { __index = {
-   log = {
-      test = lvtext,
-      generated = logext,
-      reference = tlgext,
-      expectation = lveext,
-      compare = compare_tlg,
-      rewrite = rewrite_log,
-   },
-   pdf = {
-      test = pvtext,
-      generated = pdfext,
-      reference = tpfext,
-      rewrite = rewrite_pdf,
-   },
-}})
+test_types = test_types or { }
+test_types.log = test_types.log or {
+  test = lvtext,
+  generated = logext,
+  reference = tlgext,
+  expectation = lveext,
+  compare = compare_tlg,
+  rewrite = rewrite_log,
+}
+test_types.pdf = test_types.pdf or {
+  test = pvtext,
+  generated = pdfext,
+  reference = tpfext,
+  rewrite = rewrite_pdf,
+}
+
 test_order = test_order or {"log", "pdf"}
 
 -- Manifest options
