4df7ddebe1dbc9f7a165652d76e2c753f47f1cd2
LAURENS Jérôme <jerome.laurens@u-bourgogne.fr>
2021-01-28 20:56:09 +0100

    Update l3build.lua

--- l3build.lua
+++ l3build.lua
@@ -159,7 +159,6 @@ if options["target"] == "check" then
         print("\n  Check failed with difference files")
         local testdir = testdir
         if config ~= "build" then
-          resultdir = resultdir .. "-" .. config
           testdir = testdir .. "-" .. config
         end
         for _,i in ipairs(filelist(testdir,"*" .. os_diffext)) do
