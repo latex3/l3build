cecb6263b734eaf98f8bd5ae4636d3d30371d368
Joseph Wright <joseph@texdev.net>
2024-03-05 13:32:18 +0000

    Auto-generate man file

--- build.lua
+++ build.lua
@@ -97,6 +97,47 @@ function tag_hook(tagname)
   os.execute('git commit -a -m "Step release tag"')
 end
 
+-- Auto-generate a .1 file from the help
+function  docinit_hook()
+  local find = string.find
+  local insert = table.insert
+  local open = io.open
+
+  local f = open("README.md","rb")
+  local readme = f:read("*all")
+  local date_start,date_end = find(readme,"%d%d%d%d%p%d%d%p%d%d")
+
+  local man_t = {}
+  insert(man_t,'.TH ' .. string.upper(module) .. ' 1 "'
+    .. readme:sub(date_start,date_end) .. '" "LaTeX"\n')
+  insert(man_t,(".SH NAME\n" .. module .. "\n"))
+  insert(man_t,(".SH SYNOPSIS\n Usage " .. module .. " <cmd> [<options>] [<names>]\n"))
+  insert(man_t,".SH DESCRIPTION")
+
+  local _,desc_start = find(readme,"Overview\n--------")
+  local desc_end,_ = find(readme,"Issues")
+
+  local overview = readme:sub(desc_start + 2,desc_end - 2):gsub("[`_]","")
+  insert(man_t,overview)
+
+  local cmd = "texlua ./" .. module .. ".lua --help"
+  local f = assert(io.popen(cmd,"r"))
+  local help_text = assert(f:read("*a"))
+  f:close()
+
+  insert(man_t,(help_text:gsub("\nUsage.*spec>]\n\n","")
+  :gsub("Valid commands",".SH COMMANDS\nValid commands")
+  :gsub("Valid options",".SH OPTIONS\nValid options")
+  :gsub("Full manual",'.SH "SEE ALSO"\nFull manual')
+  :gsub("Bug tracker","\nBug tracker")
+  :gsub("Copyright",".SH AUTHORS\nCopyright")))
+
+  f = assert(open(module .. ".1","wb"))
+  f:write((table.concat(man_t,"\n"):gsub("\n$","")))
+  f:close()
+  return 0
+end
+
 if not release_date then
   dofile("./l3build.lua")
 end
