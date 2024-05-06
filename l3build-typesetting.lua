5bc4936f5fe729bf48a9af6ea59de450969943e3
Joseph Wright <joseph@texdev.net>
2024-05-06 08:33:20 +0100

    Ensure errorlevel is always set up

--- l3build-typesetting.lua
+++ l3build-typesetting.lua
@@ -181,8 +181,9 @@ function docinit_hook() return 0 end
 -- Typeset all required documents
 -- Uses a set of dedicated auxiliaries that need to be available to others
 function doc(files)
+  local errorlevel = 0
   if not options["rerun"] then
-    local errorlevel = docinit()
+    errorlevel = docinit()
   end
   if errorlevel ~= 0 then return errorlevel end
   local done = {}
